443534c33903b7d0dfa5342cfea6dcd212b4bffe
Eric Laursen <ericjlaursen@gmail.com>
2021-03-09 20:23:55 -0500

    header + main elements

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,11 @@
 import React, { Fragment } from 'react';
 import './index.scss';
 
-const Layout = ({ children }) => <Fragment>{children}</Fragment>;
+const Layout = ({ children }) => (
+  <Fragment>
+    <header></header>
+    <main>{children}</main>
+  </Fragment>
+);
+
+export default Layout;
