c78af37205e31f8538931259652f5910426e9eaf
Eric Laursen <ericjlaursen@gmail.com>
2021-03-11 07:05:15 -0500

    react-helmet header component

---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+export default () => (
+  <Helmet>
+    <html lang="en" />
+    <title>imagineLife</title>
+    <meta name="description" content="site description" />
+  </Helmet>
+);
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import './index.scss';
 
+// Components
 import Header from './../header';
+import PageHeader from './../PageHeader';
 
 const Layout = ({ children }) => (
   <Fragment>
+    <PageHeader />
     <Header />
     <main>{children}</main>
   </Fragment>
