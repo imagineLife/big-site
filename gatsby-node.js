ec097ebe8cf6b166af98c96ffc931a32adbdf249
Eric Laursen <ericjlaursen@gmail.com>
2021-03-13 19:11:24 -0500

    init gatsby-node config

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const res = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (res.errors) {
+    reporter.panic(`failed to create posts ->`, res.errors);
+  }
+
+  const posts = res.data.allMdx.nodes;
+
+  posts.forEach(post => {
+    actions.createPage({
+      path: post.frontmatter.slug,
+      component: require.resolve('./src/templates/post.js'),
+      context: {
+        slug: `/${post.frontmatter.slug}/`,
+      },
+    });
+  });
+};
