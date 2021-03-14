7bb6377e529237bd3fd9d1077582d87e7e8639af
Eric Laursen <ericjlaursen@gmail.com>
2021-03-13 19:36:38 -0500

    pulls content from slug

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from './../components/layout';
 
-const PostTemplate = () => (
-  <Layout>
-    <p>Posted by Me</p>
-    <p>...content here</p>
-    <Link to="/">&larr; back to posts</Link>
-  </Layout>
-);
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => {
+  console.log('post');
+  console.log(post.body);
+
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      <p>Posted by Me</p>
+      <MDXRenderer>{post.body}</MDXRenderer>
+      <Link to="/">&larr; back to posts</Link>
+    </Layout>
+  );
+};
 
 export default PostTemplate;
