dca96ab41ab96fd6a089e3a46a3df4df9c6c0f13
Eric Laursen <ericjlaursen@gmail.com>
2021-03-20 14:47:13 -0400

    more hero styling

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './index.scss';
 import { Link } from 'gatsby';
 
-function HeroWrapper({ children }) {
-  return <div id="hero">{children}</div>;
+function DivWrapper({ id, children }) {
+  return <div id={id}>{children}</div>;
 }
 
 function Hero() {
   return (
-    <HeroWrapper>
-      <h1>Blog</h1>
-      <p>
-        <Link to="/about">About Me &rarr;</Link>
-      </p>
-    </HeroWrapper>
+    <DivWrapper id="hero">
+      <DivWrapper id="text-wrapper">
+        <h1>Blog</h1>
+        <p>
+          <Link to="/about">About Me &rarr;</Link>
+        </p>
+      </DivWrapper>
+    </DivWrapper>
   );
 }
 
