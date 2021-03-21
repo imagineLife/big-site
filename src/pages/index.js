e6a9d142249d0f9e242f9a2a770c701e772f942d
Eric Laursen <ericjlaursen@gmail.com>
2021-03-21 08:35:18 -0400

    remove logs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import './index.scss';
 
 // Components
-import { Link } from 'gatsby';
 import Layout from './../components/layout';
 import Hero from './../components/hero';
 
@@ -12,8 +11,6 @@ import getPosts from './../hooks/get-posts';
 import PostPreview from './../components/PostPreview';
 const Index = () => {
   const blogPosts = getPosts();
-  // console.log('blogPosts');
-  // console.log(blogPosts);
 
   return (
     <Fragment>
