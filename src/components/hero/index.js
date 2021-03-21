2f553e2cff04aa9c96f15ffc53917f5e1ed76b03
Eric Laursen <ericjlaursen@gmail.com>
2021-03-21 08:35:46 -0400

    new dependency, fancy image loading + class + styles

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import './index.scss';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import DivWrapper from './../divWrapper';
+import BackgroundImage from 'gatsby-background-image';
 
 function Hero() {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "farm.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <DivWrapper id="hero">
+    <BackgroundImage
+      Tag="section"
+      fluid={image.sharp.fluid}
+      fadeIn="soft"
+      style={{
+        backgroundPosition: `top 20% center`,
+        backgroundSize: 'cover',
+        height: `50vh`,
+      }}
+      className="hero"
+    >
       <DivWrapper id="text-wrapper">
         <h1>Blog</h1>
         <p>
           <Link to="/about">About Me &rarr;</Link>
         </p>
       </DivWrapper>
-    </DivWrapper>
+    </BackgroundImage>
   );
 }
 
