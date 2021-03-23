c4ac286d71fc8e6e0761f60c01dd1e071902885a
Eric Laursen <ericjlaursen@gmail.com>
2021-03-23 19:45:27 -0400

    conidtional coverImage

--- src/components/PostPreview/index.js
+++ src/components/PostPreview/index.js
@@ -3,17 +3,24 @@ import './index.scss';
 import { Link } from 'gatsby';
 import GatsbyImage from 'gatsby-image';
 
-const PostPreview = ({ title, slug, excerpt, coverImage, coverAlt }) => (
-  <article className="post-preview">
-    <Link to={slug} className="image">
-      <GatsbyImage alt={coverAlt} />
-    </Link>
-    <h3>
-      <Link to={slug}>{title}</Link>
-    </h3>
-    <p>{excerpt}</p>
-    <Link to={slug}>Read Post &rarr;</Link>
-  </article>
-);
+const PostPreview = ({ title, slug, excerpt, coverImage, coverAlt }) => {
+  return (
+    <article className="post-preview">
+      <Link to={slug} className="image">
+        {coverImage?.childImageSharp?.fluid && (
+          <GatsbyImage
+            alt={coverAlt}
+            fluid={coverImage?.childImageSharp.fluid}
+          />
+        )}
+      </Link>
+      <h3>
+        <Link to={slug}>{title}</Link>
+      </h3>
+      <p>{excerpt}</p>
+      <Link to={slug}>Read Post &rarr;</Link>
+    </article>
+  );
+};
 
 export default PostPreview;
