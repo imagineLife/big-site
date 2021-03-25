8e957a194ba4dbc6c38224149f8139358c55067b
Eric Laursen <ericjlaursen@gmail.com>
2021-03-25 06:19:23 -0400

    wp-bundle-analyzer config & script

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,14 @@ module.exports = {
         path: `images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
