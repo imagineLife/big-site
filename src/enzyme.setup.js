9e8dbf531742e909782c4dd3a405da85d1b9112e
Eric Laursen <ericjlaursen@gmail.com>
2021-03-26 07:08:56 -0400

    enzyme setuf file

---
+++ src/enzyme.setup.js
@@ -0,0 +1,9 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+//mock fetch in jest tests
+global.fetch = require('jest-fetch-mock');
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
