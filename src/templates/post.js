fea8239f3d21f25487074b741faeffc7751f6cda
Eric Laursen <ericjlaursen@gmail.com>
2021-03-28 17:50:40 -0400

    comment unused import, make css styling more broad

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { MDXProvider } from '@mdx-js/react';
+// import { MDXProvider } from '@mdx-js/react';
 import Layout from './../components/layout';
 import IngredientList from './../components/ingredientList';
 
