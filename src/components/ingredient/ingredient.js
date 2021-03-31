963d75b08d6595bafa646a8f854c2a10a112fe18
Eric Laursen <ericjlaursen@gmail.com>
2021-03-31 19:31:11 -0400

    space between spans

--- src/components/ingredient/ingredient.js
+++ src/components/ingredient/ingredient.js
@@ -4,7 +4,7 @@ import './index.scss';
 function Ingredient({ className, ingredient, amt }) {
   return (
     <li className={`ingredient ${className || ''}`}>
-      <span className="ing">{ingredient}</span>
+      <span className="ing">{ingredient}</span>{' '}
       {amt && <span className="amt">({amt})</span>}
     </li>
   );
