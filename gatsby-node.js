5d15bd316ce7c888efb4500a772d096431c46987
Eric Laursen <ericjlaursen@gmail.com>
2021-04-02 17:08:28 -0400

    renders page from post + recipe data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,33 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
   // reporter is a console interface
   const res = await graphql(`
+    fragment postpart on PostsJson {
+      slug
+      title
+      excerpt
+    }
+
+    fragment recipepart on RecipesJson {
+      slug
+      title
+      excerpt
+    }
+
+    query {
+      posts: allPostsJson {
+        data: nodes {
+          ...postpart
+        }
+      }
+      recipes: allRecipesJson {
+        data: nodes {
+          ...recipepart
+        }
+      }
+    }
+  `);
+  /*
+    allMDX Query
     query {
       allMdx {
         nodes {
@@ -10,27 +37,36 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
         }
       }
     }
-  `);
+  */
 
   if (res.errors) {
     reporter.panic(`failed to create posts ->`, res.errors);
   }
 
-  // pull post nodes from graphql obj
-  const posts = res.data.allMdx.nodes;
+  const {
+    recipes: { data: recipeData },
+    posts: { data: postData },
+  } = res.data;
+  console.log('recipeData');
+  console.log(recipeData);
+  console.log('postData');
+  console.log(postData);
 
-  posts.forEach(post => {
+  // pull post nodes from graphql obj
+  // const posts = res.data.allMdx.nodes;
+
+  [...postData, ...recipeData].forEach(post => {
     // console.log('post');
     // console.log(post.frontmatter.slug);
 
     actions.createPage({
       // where the browser can access the page
-      path: post.frontmatter.slug,
+      path: post.slug,
       // What component will pass the mdx file to
       component: require.resolve('./src/templates/post.js'),
       // name the url slug
       context: {
-        slug: post.frontmatter.slug,
+        slug: post.slug,
       },
     });
   });
