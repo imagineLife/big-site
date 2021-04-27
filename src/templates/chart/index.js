07c652fc848d6f52274035e008d03dd463f35cbe
Eric Laursen <ericjlaursen@gmail.com>
2021-04-27 06:35:05 -0400

    init input + displayed input vals

--- src/templates/chart/index.js
+++ src/templates/chart/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { graphql } from 'gatsby';
 import './index.scss';
 
@@ -32,71 +32,40 @@ export default function ChartsTemplate(apiData) {
     '%c Charts Template here!',
     'background-color: blue; color: white;',
   );
+
+  const [xDomain, setXdomain] = useState(xdomain);
+  console.log('xDomain');
+  console.log(xDomain);
+
   return (
     <main className="chart-detail">
       <h1>{title || 'A Chart'}</h1>
       <section className="scroll-box">
         <section className="chart-column d-ib b-dev">
           <div id="viz-box">
-            <svg className="chart-detail" width="400" height="400"></svg>
+            <svg className="chart-detail" width="400" height="400">
+              {xDomain.map((itm, idx) => (
+                <text key={`x-domain-${idx}`} y={(idx + 1) * 25}>
+                  {itm}
+                </text>
+              ))}
+            </svg>
           </div>
         </section>
         <section className="chart-description-column d-ib b-dev">
-          <pre>
-            <code>{`<script>
-
-
-// set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// Parse the Data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
-
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Country; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 13000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y));
-
-// Bars
-svg.selectAll("mybar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.Country); })
-    .attr("y", function(d) { return y(d.Value); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.Value); })
-    .attr("fill", "#69b3a2")
-
-})
-
-</script>`}</code>
-          </pre>
+          <h3>Chart Props</h3>
+          <article className="chart-prop">
+            <label htmlFor="x-domain">X Domain</label>
+            <input
+              type="text"
+              name="x-domain"
+              id="x-domain"
+              value={xDomain.map((elm, elmIdx) => `${elm}`)}
+              onChange={e => {
+                setXdomain([e.target.value]);
+              }}
+            ></input>
+          </article>
         </section>
       </section>
     </main>
