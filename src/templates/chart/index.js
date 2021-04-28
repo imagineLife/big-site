b7bcc9dc80a46869e2812a83dc7d3bc30b0c4cfa
Eric Laursen <ericjlaursen@gmail.com>
2021-04-27 21:27:50 -0400

    init data values display

--- src/templates/chart/index.js
+++ src/templates/chart/index.js
@@ -30,11 +30,20 @@ export default function ChartsTemplate(apiData) {
   } = apiData;
 
   const [xDomain, setXdomain] = useState(xdomain);
+  const [yDomain, setYdomain] = useState(ydomain);
+  const [localVals, setLocalVals] = useState(values);
+  const [margins, setMargins] = useState({
+    top: 20,
+    right: 5,
+    bottom: 20,
+    left: 20,
+  });
 
   return (
     <main className="chart-detail">
       <h1>{title || 'A Chart'}</h1>
       <section className="scroll-box">
+        {/* Left Column */}
         <section className="chart-column d-ib b-dev">
           <div id="viz-box">
             <svg className="chart-detail" width="400" height="400">
@@ -46,18 +55,62 @@ export default function ChartsTemplate(apiData) {
             </svg>
           </div>
         </section>
+
+        {/* Right Column */}
         <section className="chart-description-column d-ib b-dev">
           <h3>Chart Props</h3>
+          {/* Data Values */}
           <article className="chart-prop">
-            <label htmlFor="x-domain">X Domain</label>
+            <p>Data Values</p>
+            <table>
+              <tbody>
+                <tr>
+                  <td>X</td>
+                  <td>Y</td>
+                </tr>
+                {localVals.map((val, vIdx) => (
+                  <tr key={`data-val-${vIdx}`}>
+                    <td>{val.x}</td>
+                    <td>{val.y}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <br />
+            <dfn>
+              The Y Domain represents the minimin and maximum values that the
+              y-axis displays.
+            </dfn>
+          </article>
+          ;{/* Y-Domain */}
+          <article className="chart-prop">
+            <label className={`y-domain`} htmlFor="y-domain">
+              Y Domain
+            </label>
             <input
+              className={`y-domain`}
+              type="text"
+              name="y-domain"
+              id="y-domain"
+              value={yDomain.map((elm, elmIdx) => `${elm}`)}
+            />
+            <br />
+            <dfn>
+              The Y Domain represents the minimin and maximum values that the
+              y-axis displays.
+            </dfn>
+          </article>
+          {/* X-Domain */}
+          <article className="chart-prop">
+            <label className={`x-domain`} htmlFor="x-domain">
+              X Domain
+            </label>
+            <input
+              className={`x-domain`}
               type="text"
               name="x-domain"
               id="x-domain"
               value={xDomain.map((elm, elmIdx) => `${elm}`)}
-              onChange={e => {
-                setXdomain([e.target.value]);
-              }}
             />
             <br />
             <dfn>
