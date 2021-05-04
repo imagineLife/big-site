3ecd496f8260512bce8ccf5ec8c744a0d5bfa322
Eric Laursen <ericjlaursen@gmail.com>
2021-05-03 20:35:19 -0400

    rename file, adjust strengths root file

--- src/pages/strengths/index.js
+++ src/pages/strengths/index.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import { Link, useStaticQuery, query } from 'gatsby';
 import getStrengths from './../../hooks/get-strengths';
+
+// Components
 import PostPreview from './../../components/PostPreview';
+import Layout from './../../components/layout';
 
 export default function Strengths() {
   const strengthsSlugs = getStrengths();
 
   return (
-    <section id="strengths-list">
-      <h1>Personality & Strengths</h1>
-      <p>
-        Welcome to this section of my blog, highlighting some thoughts and
-        experiences about personalities and{' '}
-        <Link
-          target="_blank"
-          to="https://www.gallup.com/cliftonstrengths/en/home.aspx"
-        >
-          StrengthsFinder
-        </Link>
-        .
-      </p>
+    <Layout>
+      <section id="strengths-list">
+        <h1>Personality & Strengths</h1>
+        <p>
+          Welcome to this section of my blog, highlighting some thoughts and
+          experiences about personalities and{' '}
+          <Link
+            target="_blank"
+            to="https://www.gallup.com/cliftonstrengths/en/home.aspx"
+          >
+            StrengthsFinder
+          </Link>
+          .
+        </p>
 
-      <div id="strengths-post-list">
-        {strengthsSlugs?.map(({ title, excerpt, slug }, idx) => (
-          <PostPreview
-            key={`strengths-post-preview-${idx}`}
-            {...{ title, excerpt, slug }}
-          />
-        ))}
-      </div>
-    </section>
+        <div id="strengths-post-list">
+          {strengthsSlugs?.map(({ title, excerpt, slug }, idx) => (
+            <PostPreview
+              key={`strengths-post-preview-${idx}`}
+              {...{ title, excerpt, slug }}
+            />
+          ))}
+        </div>
+      </section>
+    </Layout>
   );
 }
