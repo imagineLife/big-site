670f8e34abdc70436a805db9b998a28f0bad2d97
Eric Laursen <ericjlaursen@gmail.com>
2021-05-06 07:02:17 -0400

    remove comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,13 @@ import './index.scss';
 // Components
 import Layout from './../components/layout';
 import Hero from './../components/hero';
+import PostPreview from './../components/PostPreview';
 
 // helper fns
 import getPosts from './../hooks/get-posts';
-// import getRecipes from './../hooks/get-recipes';
 
-import PostPreview from './../components/PostPreview';
 const Index = () => {
   const blogPosts = getPosts();
-  console.log('blogPosts');
-  console.log(blogPosts);
 
   return (
     <Fragment>
