531d928fac389dc39c2c66341b52d1039e97deff
Eric Laursen <ericjlaursen@gmail.com>
2021-06-26 07:44:06 -0400

    begin gathering prev/next page link data

--- src/templates/markdown/index.js
+++ src/templates/markdown/index.js
@@ -1,28 +1,60 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import './index.scss';
 
 export default function Template({
   data: {
-    markdownRemark: { overview, content },
+    pageData: { overview, content },
+    pageSummaries: { pages },
   },
 }) {
+  const footerLinks = pages.reduce(
+    (resArr, { page: { overview: pg } }, pgIdx) => {
+      // get previous, current, && next page details
+      if (
+        pgIdx !== overview.order &&
+        pgIdx !== overview.order - 1 &&
+        pgIdx !== overview.order + 1
+      )
+        return resArr;
+      else {
+        return [...resArr, pg];
+      }
+    },
+    [],
+  );
+  console.log('footerLinks');
+  console.log(footerLinks);
+
   return (
-    <div
-      className="scrum-container"
+    <main
+      className="md-wrapper"
       dangerouslySetInnerHTML={{ __html: content }}
-    ></div>
+    ></main>
   );
 }
 
-export const pageQuery = graphql`
+export const pgQuery = graphql`
   query ScrumBySlug($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+    pageData: markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       overview: frontmatter {
         slug
         title
         tags
+        order
       }
       content: html
+    }
+    pageSummaries: allMarkdownRemark(sort: { fields: frontmatter___order }) {
+      pages: edges {
+        page: node {
+          overview: frontmatter {
+            slug
+            title
+            order
+          }
+        }
+      }
     }
   }
 `;
