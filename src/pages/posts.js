3045d09b131bc85e33ce6fba80b715b6914ea611
Eric Laursen <ericjlaursen@gmail.com>
2021-07-04 12:54:17 -0400

    remove unused lines for now

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -6,23 +6,23 @@ import Layout from './../components/layout';
 import Hero from './../components/hero';
 
 // helper fns
-import getPosts from './../hooks/get-posts';
+// import getPosts from './../hooks/get-posts';
 // import getRecipes from './../hooks/get-recipes';
 
 import PostPreview from './../components/PostPreview';
 const Index = () => {
-  const blogPosts = getPosts();
-  console.log('blogPosts');
-  console.log(blogPosts);
+  // const blogPosts = getPosts();
+  // console.log('blogPosts');
+  // console.log(blogPosts);
 
   return (
     <Fragment>
       <Hero />
       <Layout>
         <h2>BlogPosts</h2>
-        {blogPosts.map(bp => (
+        {/* {blogPosts.map(bp => (
           <PostPreview {...bp} key={bp.slug} />
-        ))}
+        ))} */}
       </Layout>
     </Fragment>
   );
