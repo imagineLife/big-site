b5a4aeb064bfa825ff6a5ef11233ba8a79d8ce7b
Eric Laursen <ericjlaursen@gmail.com>
2021-07-17 11:52:29 -0400

    another example

--- content/mongo/aggregations/filtermovies.js
+++ content/mongo/aggregations/filtermovies.js
@@ -40,3 +40,22 @@
     $project: { _id: 0, title: 1, rated: 1 },
   },
 ];
+
+/*
+Using aggregation, 
+  find a count of the number of movies 
+  that have a title composed of one word. 
+  To clarify, "Cinderella" and "3-25" should count, 
+  where as "Cast Away" would not.
+*/
+db.movies
+  .aggregate([
+    {
+      $project: {
+        titleWordCount: { $size: { $split: ['$title', ' '] } },
+        _id: 0,
+      },
+    },
+    { $match: { titleWordCount: { $lt: 2 } } },
+  ])
+  .itcount();
