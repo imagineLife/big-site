677b1ef20ea5352b4f9907710ae25a159bd13d0f
Eric Laursen <ericjlaursen@gmail.com>
2021-07-17 16:41:42 -0400

    more complex pipeline

--- content/mongo/aggregations/filtermovies.js
+++ content/mongo/aggregations/filtermovies.js
@@ -93,3 +93,106 @@ db.movies.aggregate([
     },
   },
 ]);
+
+/*  
+  Get movies where the same person is a writer AND an actor AND a director
+*/
+db.movies.aggregate([
+  {
+    $match: {
+      cast: { $elemMatch: { $exists: true } },
+      directors: { $elemMatch: { $exists: true } },
+      writers: { $elemMatch: { $exists: true } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      cast: 1,
+      directors: 1,
+      writers: {
+        $map: {
+          input: '$writers',
+          as: 'writer',
+          in: {
+            $arrayElemAt: [
+              {
+                $split: ['$$writer', ' ('],
+              },
+              0,
+            ],
+          },
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      labor_of_love: {
+        $gt: [
+          { $size: { $setIntersection: ['$cast', '$directors', '$writers'] } },
+          0,
+        ],
+      },
+    },
+  },
+  {
+    $match: { labor_of_love: true },
+  },
+  {
+    $count: 'labors of love',
+  },
+]);
+/*
+  Pipeline Breakdown
+  - {
+    $match: {
+      cast: { $elemMatch: { $exists: true } },
+      directors: { $elemMatch: { $exists: true } },
+      writers: { $elemMatch: { $exists: true } },
+    },
+  }
+    - only return elements that HAVE content in those 3 arrays
+  - {
+    $project: {
+      _id: 0,
+      cast: 1,
+      directors: 1,
+      writers: {
+        $map: {
+          input: '$writers',
+          as: 'writer',
+          in: {
+            $arrayElemAt: [
+              {
+                $split: ['$$writer', ' ('],
+              },
+              0,
+            ],
+          },
+        },
+      },
+    },
+  },
+    - only return desired fields, and on writers field, only return the writes name and exclude the (special words here)
+  - {
+    $project: {
+      labor_of_love: {
+        $gt: [
+          { $size: { $setIntersection: ['$cast', '$directors', '$writers'] } },
+          0,
+        ],
+      },
+    },
+  },
+    - create a SINGLE field called labor_of_love, which returns true or false
+      based on the size of the result of the setIntersection calculation: when 0 false, when match true
+  - {
+    $match: { labor_of_love: true },
+  }
+    - only return TRUE matches
+  - {
+    $count: 'labors of love',
+  }
+    - calc a count of items
+*/
