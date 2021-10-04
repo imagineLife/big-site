7f4b4f4f0787a609ea6bc540ffd6a66ae793de1e
Eric Laursen <ericjlaursen@gmail.com>
2021-10-04 06:58:12 -0400

    adjust section mapping

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,39 @@ import Hero from './../components/hero';
 import BlogSectionPreview from './../components/BlogSectionPreview';
 import useSections from './../hooks/use-sections';
 
+let FlexRowDiv = ({ children }) => <div className="flex-row">{children}</div>;
+
 const Index = () => {
   const sections = useSections();
+  // store sections in arr of arrs, 1 nested arr per ui "row"
+  // 4 "sections" per row
+  let sectionRows = [[], []];
+  sections.forEach((s, sidx) => {
+    if (sidx <= 3) sectionRows[0].push(s);
+    if (sidx > 3) sectionRows[1].push(s);
+  });
+
   return (
     <Fragment>
       <Hero />
       <Layout>
         <main className="centered">
           <h2>A collection of writings on topics I've been working with</h2>
-          <section className="flex col">
-            {sections.map((s, sidx) => (
-              <BlogSectionPreview key={`content-section-${sidx}`} {...s} />
+          <section className="flex-col">
+            {sectionRows.map((row, rowIdx) => (
+              <FlexRowDiv key={`section-row-${rowIdx}`}>
+                {row.map((blogSection, bsIdx) => (
+                  <BlogSectionPreview
+                    key={`content-section-${rowIdx}-${bsIdx}`}
+                    {...blogSection}
+                  />
+                ))}
+              </FlexRowDiv>
             ))}
+            {/* {sections.map((s, sidx) => (
+              <BlogSectionPreview key={`content-section-${sidx}`} {...s} />
+            ))} */}
           </section>
-          {/* <Link to="/febs">A Frontend Build System</Link> */}
           {/* <Link to="">Developing Engineer Competencies toward career growth</Link> */}
           <Link to="/misc">Miscellaneous</Link>
         </main>
