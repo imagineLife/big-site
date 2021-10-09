d6f8fc1949048491d75969e419b332529063500a
Eric Laursen <ericjlaursen@gmail.com>
2021-10-08 21:40:20 -0400

    comment stylesheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,2 @@
 require('prismjs/themes/prism-solarizedlight.css');
-require('prismjs/plugins/command-line/prism-command-line.css');
+// require('prismjs/plugins/command-line/prism-command-line.css');
