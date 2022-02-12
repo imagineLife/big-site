da2d0fe93b1ef97bb8b13aafe18d687f9a9e533f
Eric Laursen <ericjlaursen@gmail.com>
2022-02-12 18:01:56 -0500

    gtag config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,28 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-WZNFNRGGSR', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Defaults to https://www.googletagmanager.com
+          // origin: 'YOUR_SELF_HOSTED_ORIGIN',
+        },
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
