650d2c974122343d7e18ab71985a9415bace3af0
Eric Laursen <ericjlaursen@gmail.com>
2022-02-14 07:21:01 -0500

    useWindowSize

---
+++ src/hooks/useWindowSize/index.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowSize;
+}
+
+export default useWindowSize;
