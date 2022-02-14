c72a928089d8cd45b05a256c58dcf33e4187a4e1
Eric Laursen <ericjlaursen@gmail.com>
2022-02-14 18:53:39 -0500

    new styles, less complicated rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,13 @@ import Layout from './../components/layout';
 import Hero from './../components/hero';
 import BlogSectionPreview from './../components/BlogSectionPreview';
 import useSections from './../hooks/use-sections';
+import useWindowSize from './../hooks/useWindowSize';
 
 let FlexRowDiv = ({ children }) => <div className="flex-row">{children}</div>;
 
 const Index = () => {
+  const windowSize = useWindowSize();
+
   const sections = useSections();
   // store sections in arr of arrs, 1 nested arr per ui "row"
   // 4 "sections" per row
@@ -22,7 +25,7 @@ const Index = () => {
 
   return (
     <Fragment>
-      <Hero />
+      <Hero windowWidth={windowSize?.width} />
       <Layout>
         <main className="centered">
           <p>
@@ -33,20 +36,13 @@ const Index = () => {
           </p>
           <br />
           <h2>A collection of writings on topics I've been working with</h2>
-          <section className="flex-col flex-center">
-            {sectionRows.map((row, rowIdx) => (
-              <FlexRowDiv key={`section-row-${rowIdx}`}>
-                {row.map((blogSection, bsIdx) => (
-                  <BlogSectionPreview
-                    key={`content-section-${rowIdx}-${bsIdx}`}
-                    {...blogSection}
-                  />
-                ))}
-              </FlexRowDiv>
+          <section className="flex-row flex-center flex-wrap">
+            {sections.map((blogSection, rowIdx) => (
+              <BlogSectionPreview
+                key={`content-section-${rowIdx}`}
+                {...blogSection}
+              />
             ))}
-            {/* {sections.map((s, sidx) => (
-              <BlogSectionPreview key={`content-section-${sidx}`} {...s} />
-            ))} */}
           </section>
           {/* <Link to="">Developing Engineer Competencies toward career growth</Link> */}
           <footer className="flex-col">
