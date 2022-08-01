06861dc1732c1b3e0e0925cf27bf6625e92e2d11
Eric Laursen <ericjlaursen@gmail.com>
2022-08-01 10:38:49 -0400

    include more in-depth links

--- src/pages/k8s.js
+++ src/pages/k8s.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
     query={graphql`
       query k8sTOC {
         k8s: allMarkdownRemark(
-          filter: { frontmatter: { slug: { regex: "/k8s/" }, parentDir: { regex: "/k8s$/" } } }
+          filter: { frontmatter: { slug: { regex: "/k8s/" }, parentDir: { regex: "/k8s/" } } }
           sort: { fields: frontmatter___order }
         ) {
           pages: edges {
@@ -33,14 +33,49 @@ const IndexPage = () => (
       }
     `}
     render={({ k8s: { pages } }) => {
+      let rootPages = []
+      let inDepthPages = []
+      // categorize into "Getting Started" and "In-Depth"
+      pages.forEach((p,pidx) => { 
+        if (!p.page.overview.slug.includes('in-depth')) {
+          rootPages.push(p);
+        } else { 
+          inDepthPages.push(p)
+        }
+      })
       return (
         <Fragment>
           <Hero />
           <Layout>
             <section className="toc-wrapper">
               <h1>K8s</h1>
-              <h2 title="Thanks to Brian Holt from Frontend Masters for Sparking some Curiosity here!">Getting Started</h2>
-              {pages.map(
+              <h2 title="Thanks to Brian Holt from Frontend Masters for Sparking some Curiosity here!">
+                Getting Started
+              </h2>
+              {rootPages.map(
+                (
+                  {
+                    page: {
+                      overview: { slug, title, excerpt },
+                    },
+                  },
+                  pageIdx,
+                ) => {
+                  return (
+                    <div className="toc-card" key={`k8s-toc-${pageIdx}`}>
+                      <Link to={`/${slug}`} className="title">
+                        {title}
+                      </Link>
+                      <p className="content">{excerpt}</p>
+                    </div>
+                  );
+                },
+              )}
+              <br />
+              <br />
+              <br />
+              <h2>More In-Depth</h2>
+              {inDepthPages.map(
                 (
                   {
                     page: {
