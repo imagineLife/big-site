7836160eecd4b9b2eef3b5faeef6265d1fa7ef7b
Eric Laursen <ericjlaursen@gmail.com>
2022-10-11 21:24:18 -0400

    remove comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,12 +51,8 @@ const Index = () => {
             </Link>{' '}
             of work including web apps, technologies, and certifications
           </p>
-          {/* <Link to="">Developing Engineer Competencies toward career growth</Link> */}
           <footer className="flex-col">
             <Link to="/misc">Miscellaneous</Link>
-            {/* <Link to="/charts">
-              Charts: A Growing folio of data visualization work
-            </Link> */}
           </footer>
         </main>
       </Layout>
