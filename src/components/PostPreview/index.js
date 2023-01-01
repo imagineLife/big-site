a91976656dcd9bcfb0152d9aaca520b9f1f9bdd1
Eric Laursen <ericjlaursen@gmail.com>
2023-01-01 12:14:48 -0500

    commen the image

--- src/components/PostPreview/index.js
+++ src/components/PostPreview/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.scss';
 import { Link } from 'gatsby';
-import GatsbyImage from 'gatsby-image';
+// import GatsbyImage from 'gatsby-image';
 
 const PostPreview = ({ title, slug, excerpt, coverImage, coverAlt }) => {
   const slugString = slug.charAt(0) === '/' ? slug : `/${slug}`;
@@ -11,12 +11,13 @@ const PostPreview = ({ title, slug, excerpt, coverImage, coverAlt }) => {
       {/* Optional Preview Image/Link */}
       {coverImage && (
         <Link to={slugString} className="image">
+          {/* 
           {coverImage?.childImageSharp?.fluid && (
             <GatsbyImage
               alt={coverAlt}
               fluid={coverImage?.childImageSharp.fluid}
             />
-          )}
+          )} */}
         </Link>
       )}
       <div>
@@ -27,7 +28,7 @@ const PostPreview = ({ title, slug, excerpt, coverImage, coverAlt }) => {
         <Link to={slugString}>Read Post &rarr;</Link>
       </div>
     </article>
-  );
+  )
 };
 
 export default PostPreview;
