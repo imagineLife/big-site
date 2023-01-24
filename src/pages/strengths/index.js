dcd45b31238b004fca4742271ac1d251d2b65da3
Eric Laursen <ericjlaursen@gmail.com>
2023-01-24 15:50:19 -0500

    comment the query

--- src/pages/strengths/index.js
+++ src/pages/strengths/index.js
@@ -8,29 +8,29 @@ import Hero from './../../components/hero';
 // import './scrum.scss';
 
 const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      query StrengthsTOC {
-        strengths: allMarkdownRemark(
-          sort: { frontmatter: { order: ASC } }
-          filter: {
-            frontmatter: { order: { gt: 0 }, slug: { regex: "/strengths/" } }
-          }
-        ) {
-          pages: edges {
-            page: node {
-              overview: frontmatter {
-                slug
-                title
-                excerpt
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={({ strengths: { pages } }) => {
-      return (
+  // <StaticQuery
+  //   query={graphql`
+  //     query StrengthsTOC {
+  //       strengths: allMarkdownRemark(
+  //         sort: { frontmatter: { order: ASC } }
+  //         filter: {
+  //           frontmatter: { order: { gt: 0 }, slug: { regex: "/strengths/" } }
+  //         }
+  //       ) {
+  //         pages: edges {
+  //           page: node {
+  //             overview: frontmatter {
+  //               slug
+  //               title
+  //               excerpt
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `}
+    // render={({ strengths: { pages } }) => {
+      // return (
         <Fragment>
           <Hero />
           <Layout>
@@ -40,7 +40,7 @@ const IndexPage = () => (
                 Some thoughts on how we can view ourselves and one another as
                 individuals with natural talents.
               </p>
-              {pages.map(
+              {/* {pages.map(
                 (
                   {
                     page: {
@@ -58,13 +58,13 @@ const IndexPage = () => (
                     </div>
                   )
                 }
-              )}
+              )} */}
             </section>
           </Layout>
         </Fragment>
-      )
-    }}
-  />
+      // )
+    // }}
+  // />
 )
 
 export default IndexPage;
