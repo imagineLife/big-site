ff6ccc4081838299ceb32283c76341698281353a
Eric Laursen <ericjlaursen@gmail.com>
2023-01-29 12:25:18 -0500

    media links in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,8 @@
 import './index.scss';
 import React from 'react';
 import { Link } from 'gatsby';
+import linkedIn from './../../pages/about/linkedin.svg';
+import github from "./../../pages/about/github.svg"
 
 const headerLinks = [
   {
@@ -24,6 +26,17 @@ const headerLinks = [
     to: '/node',
   }
 ];
+
+const headerContactLinks = [
+  {
+    to: "https://www.linkedin.com/in/eric-laursen-6a1b20b8/",
+    src: linkedIn,
+  },
+  {
+    to: "https://github.com/imagineLife",
+    src: github,
+  },
+]
 const Header = ({className}) => (
   <header className={`header ${className}`}>
     <Link className="styled bold" to="/">
@@ -36,6 +49,17 @@ const Header = ({className}) => (
         </Link>
       ))}
     </nav>
+    <div id="header-links-wrapper" style={{minWidth: '80px', display: 'flex'}}>
+      {headerContactLinks.map(d => (
+        <Link to={d.to} target="_blank" key={d.to} style={{
+          margin: 'auto',
+          height: '20px',
+          width: '20px',
+        }}>
+          <img style={{ width: "20px", height:'20px' }} src={d.src} />
+        </Link>
+      ))}
+    </div>
   </header>
 );
 
