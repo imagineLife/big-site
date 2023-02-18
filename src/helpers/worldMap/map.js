8f724e2f146cefabfb85030cbe6d4a3b290238cc
Eric Laursen <ericjlaursen@gmail.com>
2023-02-18 15:22:30 -0500

    legend + map updates

--- src/helpers/worldMap/map.js
+++ src/helpers/worldMap/map.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React from "react"
+import Legend from './legend'
+function colorVal(d) {
+  return d.properties.economy
+}
 
-export default function Map({countries}) { 
-  return (<p>There will be a map...</p>)
+export default function Map({ countries, colorScale }) {
+  const svgProps = {
+    height: "100vh",
+    width: " 100%",
+    fontSize: " 32pt",
+    fontFamily: " sans-serif",
+  }
+
+  return (
+    <svg className="svgWrapper" {...svgProps}>
+      <g className="map-g" pointerEvents={"all"}></g>
+      <Legend colorScale={colorScale} />
+    </svg>
+  )
 }
