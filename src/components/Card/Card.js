4cbc30ad3c6afb02cf42c3dbaca7dd6a482900a8
Eric Laursen <ericjlaursen@gmail.com>
2023-03-11 19:05:55 -0500

    optional children rendering

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './card.scss';
 
-const card = ({ title, content, className }) => (
-  <div className={`card ${className || ''}`}>
+const Card = ({ title, content, className, children }) => (
+  <div className={`card ${className || ""}`}>
     <div className="text-section">
       {title && <h3>{title}</h3>}
       {content && <p>{content}</p>}
     </div>
+    {children}
   </div>
-);
+)
 
-export default card;
+export default Card;
