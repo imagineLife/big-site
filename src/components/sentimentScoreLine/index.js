b5a4e0abb719b9895878b353f56068d1277c8a5b
Eric Laursen <ericjlaursen@gmail.com>
2023-03-18 18:42:30 -0400

    link to semiotic line docs

--- src/components/sentimentScoreLine/index.js
+++ src/components/sentimentScoreLine/index.js
@@ -6,6 +6,7 @@ import Card from "./../Card"
   Card, or no card?
   that - is the question
 */ 
+// https://semiotic.nteract.io/guides/line-chart
 export default function SentimentScoreLine({ frameProps, isLoading }) {  
   return (
     <Card loading={isLoading} title="Sentence Sentiment Scores">
