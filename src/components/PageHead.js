5e44f56d6dbfeac96beebe690ec5441848997dbf
Eric Laursen <ericjlaursen@gmail.com>
2023-03-19 19:00:11 -0400

    images in ld+json

--- src/components/PageHead.js
+++ src/components/PageHead.js
@@ -1,29 +1,48 @@
 import React, { Fragment } from 'react';
+import k8sImg from './../../images/imgs/k8s.png';
+import nodeImg from "./../../images/imgs/Node.png";
+import bashImg from "./../../images/imgs/bash.png";
+import dockerImg from "./../../images/imgs/docker.png";
+import mongoImg from "./../../images/imgs/mongoDB.png"
 
-export default function PageHead({title, excerpt, slug, words, tags }) {
-  return (
-    <Fragment>
-      <title>{title || "Jake Laursen Blog"}</title>
-      <meta name="description" content={excerpt} />
-      <meta property="og:title" content={title} />
-      <meta property="og:url" content={`http://laursen.tech/${slug}`} />
-      <script type="application/ld+json">
-        {JSON.stringify({
-          "@context": "https://schema.org",
-          "@type": "BlogPosting",
-          name: title,
-          author: {
-            "@type": "Person",
-            name: "Eric (Jake) Laursen",
-            url: "http://laursen.tech/about",
-          },
-          description: excerpt,
-          headline: title,
-          wordCount: words || null,
-          keywords: [...tags, "blog", "blog post", "blogpost"],
-          inLanguage: "en-US",
-        })}
-      </script>
-    </Fragment>
-  )
+function imgFromSlug(s) {
+  if (s.includes('k8s')) return k8sImg;
+  if (s.includes("node")) return nodeImg;
+  if (s.includes('linux')) return bashImg;
+  if (s.includes("docker")) return dockerImg;
+  if (s.includes("mongo")) return mongoImg;
+  return false
+}
+
+export default function PageHead({ title, excerpt, slug, words, tags }) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "BlogPosting",
+    name: title,
+    author: {
+      "@type": "Person",
+      name: "Eric (Jake) Laursen",
+      url: "http://laursen.tech/about",
+    },
+    description: excerpt,
+    headline: title,
+    wordCount: words || null,
+    keywords: [...tags, "blog", "blog post", "blogpost"],
+    inLanguage: "en-US",
+  }
+
+  const optionalImg = imgFromSlug(slug)
+  if (optionalImg) {
+    jsonLd.image = `http://laursen.tech${optionalImg}`
+  }
+
+    return (
+      <Fragment>
+        <title>{title || "Jake Laursen Blog"}</title>
+        <meta name="description" content={excerpt} />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={`http://laursen.tech/${slug}`} />
+        <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
+      </Fragment>
+    )
 }
