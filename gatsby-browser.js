65e413b5b33d6bfbc497e42c05357f91431acb8f
Eric Laursen <ericjlaursen@gmail.com>
2023-03-22 17:46:00 -0400

    try at ignoring scroll position

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,3 +12,18 @@ require("./src/style.css")
 require("prismjs/themes/prism-tomorrow.min.css")
 // require("prismjs/themes/prism.css")
 // require('prismjs/plugins/command-line/prism-command-line.css');
+
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  // getSavedScrollPosition,
+}) => {
+  // const { pathname } = location
+  // list of routes for the scroll-to-top-hook
+  // const scrollToTopRoutes = [`/privacy-policy`, `/page-2`]
+  // // if the new route is part of the list above, scroll to top (0, 0)
+  // if (scrollToTopRoutes.indexOf(pathname) !== -1) {
+  //   window.scrollTo(0, 0)
+  // }
+
+  return false
+}
