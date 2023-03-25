2923e02ea14a3fcbba99ebc5472cc4c0f888e768
Eric Laursen <ericjlaursen@gmail.com>
2023-03-25 13:16:39 -0400

    accommodate k8s in-depth options

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,18 +3,22 @@ import { Link } from "gatsby"
 
 import './index.scss'
 export default function Sidebar({ items, navWithSections, parentDir }) {
+  
+  let innerParentDir = parentDir;
+  if (parentDir.includes('in-depth')) innerParentDir = "k8s"
+  
   return (
     <nav aria-label={`learn-${parentDir}`} className="sidebar">
       {navWithSections &&
         navWithSections.map(arrOfSections => (
           <div key={arrOfSections.sectionName}>
-              <span>{arrOfSections.sectionName}</span>
+            <span>{arrOfSections.sectionName}</span>
             <div role="region" aria-label={arrOfSections.sectionName}>
               {arrOfSections.items.map(
                 ({ frontmatter: { shortSlug, title } }, pidx) => (
                   <Link
                     key={`${pidx}-${shortSlug}`}
-                    to={`/${parentDir}/${shortSlug}`}
+                    to={`/${innerParentDir}/${shortSlug}`}
                   >
                     {title}
                   </Link>
@@ -25,7 +29,10 @@ export default function Sidebar({ items, navWithSections, parentDir }) {
         ))}
       {!navWithSections &&
         items?.map(({ frontmatter: { shortSlug, title } }, pidx) => (
-          <Link key={`${pidx}-${shortSlug}`} to={`/${parentDir}/${shortSlug}`}>
+          <Link
+            key={`${pidx}-${shortSlug}`}
+            to={`/${innerParentDir}/${shortSlug}`}
+          >
             {title}
           </Link>
         ))}
