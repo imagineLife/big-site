857c365aa31ac663c716ce9dc6f2a89dfbf00f0b
Eric Laursen <ericjlaursen@gmail.com>
2023-03-29 17:13:04 -0400

    adjust shouldUpdateScroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,19 +13,8 @@ require("prismjs/themes/prism-tomorrow.min.css")
 // require("prismjs/themes/prism.css")
 // require('prismjs/plugins/command-line/prism-command-line.css');
 
-exports.shouldUpdateScroll = ({
-  routerProps: { location },
-  // getSavedScrollPosition,
-}) => {
-  // const { pathname } = location
-  // list of routes for the scroll-to-top-hook
-  // const scrollToTopRoutes = [`/privacy-policy`, `/page-2`]
-  // // if the new route is part of the list above, scroll to top (0, 0)
-  // if (scrollToTopRoutes.indexOf(pathname) !== -1) {
-  //   window.scrollTo(0, 0)
-  // }
-
-  return false
+exports.shouldUpdateScroll = () => {
+  window.scrollTo(0, 0)
 }
 
 exports.onInitialClientRender = () => {
