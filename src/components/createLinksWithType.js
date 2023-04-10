c727d98e0d71460322ea91edeb0713b66246163c
Eric Laursen <ericjlaursen@gmail.com>
2023-04-10 08:04:05 -0400

    remove short-slug-only from createLinksWithType

--- src/components/createLinksWithType.js
+++ src/components/createLinksWithType.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import SocialLink from './socialLink';
+import React from "react"
+import SocialLink from "./socialLink"
 
 function createLinksWithType({ thisType }) {
-  return function createSocialLinks(
-    {
-      page: {
-        overview: { slug, title, excerpt, parentDir, shortSlug },
-      },
+  return function createSocialLinks({
+    page: {
+      overview: { slug, title, excerpt, parentDir, shortSlug },
     },
-    pageIdx
-  ) {
-    let innerSlug = thisType.includes('k8s-in-depth') ? `k8s/${shortSlug}` : slug
-    // if (parentDir === 'docker') innerSlug = `docker/${innerSlug}`
-      return (
-        <SocialLink
-          slug={innerSlug}
-          title={title}
-          excerpt={excerpt}
-          key={`${thisType}-toc-${pageIdx}`}
-        />
-      )
+  }) {
+    return (
+      <SocialLink
+        slug={slug}
+        title={title}
+        excerpt={excerpt}
+        key={`${thisType}-toc-${slug}`}
+      />
+    )
   }
 }
 
