94fe1c0daa59b2c45c7b0c22870ed409b04f0408
Eric Laursen <ericjlaursen@gmail.com>
2023-06-13 06:55:34 -0400

    init

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,69 +1,53 @@
-import React from 'react';
-export default function Contact() {
-  const reg = {
-    type: "text",
-    required: true,
+import React, { Fragment } from 'react';
+import './index.scss';
+
+// Components
+import Layout from './../components/layout';
+import Hero from './../components/hero';
+
+const Index = () => {
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const myForm = event.target
+    const formData = new FormData(myForm)
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => alert("thanks for the submission!"))
+      .catch(error => alert(error))
   }
-  const inputs = [
-    {
-      name: "Name",
-      placeholder: "Thelonious Coltrane",
-      ...reg,
-    },
-    {
-      name: "Email Address",
-      placeholder: "thanks@forTheEmail",
-      ...reg,
-    },
-    {
-      ...reg,
-      name: "Message For Me",
-      placeholder: "Let Me Know anything about you!",
-      required: false,
-    },
-    {
-      ...reg,
-      type: "checkbox",
-      name: "updates",
-      label: "Get Summary update emails from me as I post more content",
-    },
-  ]
-
-  /*
-  
-     <div>
-        <label for="name-input">Name</label>
-        <input
-          id="name-input"
-          type="text"
-          name="name"
-          placeholder="Jane Doe"
-          required
-        />
-      </div>
-
-  */
 
   return (
-    <form action="" method="POST">
-      {inputs.map(({ type, name, label, placeholder, required }) => (
-        <div key={`input-${name}`}>
-          {type !== "checkbox" && (
-            <label htmlFor={`${name}-input`}>{label || name}</label>
-          )}
-          <input
-            id={`${name}-input`}
-            type={type}
-            name={name}
-            placeholder={placeholder || ""}
-            required={required}
-          />
-          {type == "checkbox" && (
-            <label htmlFor={`${name}-input`}>{label || name}</label>
-          )}
-        </div>
-      ))}
-      <input type="submit" value="Send" />
-    </form>
+    <Fragment>
+      <Hero />
+      <Layout>
+        <form name="contact" netlify onSubmit={handleSubmit}>
+          <p>
+            <label>
+              Name <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Email <input type="email" name="email" />
+            </label>
+          </p>
+
+          <p>
+            <label>
+              Message <input type="text" name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </Layout>
+    </Fragment>
   )
-}
+};
+export default Index;
