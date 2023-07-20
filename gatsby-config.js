edd2819e8869f782d0040eb8c20d8d07c318d5ab
Eric Laursen <ericjlaursen@gmail.com>
2023-07-20 07:08:01 -0400

    redo https sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -244,8 +244,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: "http://laursen.tech",
-        sitemap: "http://laursen.tech/sitemap-index.xml",
+        host: "https://laursen.tech",
+        sitemap: "https://laursen.tech/sitemap-index.xml",
         // { userAgent: "*", allow: "/" }
         policy: [
           ...robots.allow.map(d => ({
