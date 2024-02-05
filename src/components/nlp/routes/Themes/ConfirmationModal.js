df13889e37bbcfd50fbf39ee13998e3723375929
Eric Laursen <ericjlaursen@gmail.com>
2024-02-05 12:00:27 -0500

    Clear local state on confirmation

--- src/components/nlp/routes/Themes/ConfirmationModal.js
+++ src/components/nlp/routes/Themes/ConfirmationModal.js
@@ -183,6 +183,8 @@ export default function ConfirmationModal({
                 theme: localTheme || getValues().theme,
                 keywordList,
               })
+              setLocalKeywordList([])
+              setLocalKeyword()
             }
             reset()
           }}
