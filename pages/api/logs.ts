aa470984e1394ba94337137d5a8c7e94d9ccf321
Eric Laursen <ericjlaursen@gmail.com>
2025-03-04 17:50:26 -0500

    remove api dir

--- pages/api/logs.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getLogs } from './../../utils/logs/parseLogs'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const logs = await getLogs();
-  res.status(200).json(logs);
-}
