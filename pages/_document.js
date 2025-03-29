d48c80416616d9ed89cc93b6fc815c34aa59c6fe
Eric Laursen <ericjlaursen@gmail.com>
2025-03-29 11:42:15 -0400

    gsv

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="iogU_AVrpRm_CaNC120gaW3bv3wW19phU94qf8hxSZ4"
+          />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
