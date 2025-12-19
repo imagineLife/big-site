d8b9f20d15d81e39dcec5bbec23f46603ee3f810
Eric Laursen <ericjlaursen@gmail.com>
2025-12-19 18:35:30 -0500

    update deps

--- next.config.js
+++ next.config.js
@@ -1,38 +1,22 @@
-// DOCS
-// https://nextjs.org/docs/pages/api-reference/next-config-js/webpack
-
-// txt file
-const textLoader = {
-  test: /\.txt/,
-  type: 'asset/source',
-};
-
-const svgLoader = {
-  test: /\.svg$/,
-  use: ['@svgr/webpack'],
-};
+// next.config.js
 
 const nextConfig = {
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.module.rules.push(textLoader);
-    config.module.rules.push(svgLoader);
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+      '*.txt': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+    },
+  },
 
-    // Important: return the modified config
-    return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.svgrepo.com',
-        pathname: '/show/**',
-      },
+      { protocol: 'https', hostname: 'www.svgrepo.com', pathname: '/show/**' },
       {
         protocol: 'https',
         hostname: 's3-us-west-2.amazonaws.com',
@@ -43,6 +27,7 @@ const nextConfig = {
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   staticPageGenerationTimeout: 120,
 };
